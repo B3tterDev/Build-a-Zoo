2b9998648d00cb45594e3d85c5f865ef75d5ef9c
B3tterDev <nattawut.developer@gmail.com>
2025-10-01 08:31:30 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
 repeat wait() until game:IsLoaded()
-wait(15)
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local Window = Fluent:CreateWindow({
     Title = "Build a Zoo",
