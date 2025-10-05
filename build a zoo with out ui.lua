d3534cf7652f43c089669a89df6d4774ddd6a7b8
B3tterDev <nattawut.developer@gmail.com>
2025-10-05 09:13:41 +0700

    Add files via upload

---
+++ build a zoo with out ui.lua
@@ -0,0 +1,69 @@
+print('[B3tterDev] Initializing data ..')
+repeat wait() until game:IsLoaded()
+wait(30)
+print("[B3tterDev] Verification successful, this resource is READY to use")
+
+task.spawn(function()
+    while true do
+        local pets = game:GetService("Players").LocalPlayer.PlayerGui.Data.Pets:GetChildren()
+        for _, v in pairs(pets) do
+            local petModel = game:GetService("Workspace"):FindFirstChild("Pets"):FindFirstChild(v.Name)
+            if petModel and petModel:FindFirstChild("RootPart") and petModel.RootPart:FindFirstChild("RE") then
+                local args = { "Claim" }
+                petModel.RootPart.RE:FireServer(unpack(args))
+            end
+        end
+        task.wait(5)
+    end
+end)
+
+task.spawn(function()
+    while true do
+        local pets = game:GetService("Workspace"):WaitForChild("Pets")
+        for _, v in pairs(pets:GetChildren()) do
+            local petModel = pets:FindFirstChild(v.Name)
+            if petModel then
+                local animCtrl = petModel:FindFirstChild("AnimationController")
+                if animCtrl then
+                    animCtrl:Destroy()
+                end
+
+                local MutateFX = petModel:FindFirstChild("MutateFX_Inst")
+                if MutateFX then
+                    MutateFX:Destroy()
+                end
+            end
+        end
+
+        local PlayerBuiltBlocks = game:GetService("Workspace"):WaitForChild("PlayerBuiltBlocks")
+        for _, v in pairs(PlayerBuiltBlocks:GetChildren()) do
+            local builtModel = PlayerBuiltBlocks:FindFirstChild(v.Name)
+            if builtModel then
+                local animCtrl = builtModel:FindFirstChild("AnimationController")
+                if animCtrl then
+                    animCtrl:Destroy()
+                end
+
+                local MutateFX = builtModel:FindFirstChild("MutateFX_Inst")
+                if MutateFX then
+                    MutateFX:Destroy()
+                end
+            end
+        end
+
+        task.wait(30)
+    end
+end)
+
+local popupDrop = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("PopupDrop")
+if popupDrop then popupDrop:Destroy() end
+
+local Shared = require(game:GetService("ReplicatedStorage"):WaitForChild("Shared"))
+local Formatter = Shared("Format")
+local moneyStat = game:GetService("Players").LocalPlayer:WaitForChild("leaderstats"):WaitForChild("Money $")
+moneyStat.Changed:Connect(function(newValue)
+    local coinHud = game:GetService("Players").LocalPlayer.PlayerGui.OverlaySafe:WaitForChild("CoinHud")
+    local textLabel = coinHud:WaitForChild("Value")
+
+    textLabel.Text = Formatter:Number2String(newValue, "en")
+end)
