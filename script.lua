dab6f0cb43c0dc0f86c16546b743080904d2838b
B3tterDev <nattawut.developer@gmail.com>
2025-10-10 00:51:32 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -13,7 +13,7 @@ task.spawn(function()
                 petModel.RootPart.RE:FireServer(unpack(args))
             end
         end
-        task.wait(5)
+        task.wait(1)
     end
 end)
 
@@ -67,4 +67,3 @@ moneyStat.Changed:Connect(function(newValue)
 
     textLabel.Text = Formatter:Number2String(newValue, "en")
 end)
-
